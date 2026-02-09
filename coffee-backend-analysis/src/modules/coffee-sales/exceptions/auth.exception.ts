d97f59d40b964ebfdcbc81c812541b87b2fc9e26
Angeluz20 <estevao.silva015@gmail.com>
2026-02-08 23:36:28 -0300

    refactor(backend): remove duplicated module-specific exceptions

--- coffee-backend-analysis/src/modules/coffee-sales/exceptions/auth.exception.ts
+++
@@ -1,8 +0,0 @@
-import { UnauthorizedException } from '@nestjs/common';
-import { AuthMessages } from 'src/common/messages/auth.messages';
-
-export class InvalidCredentialsException extends UnauthorizedException {
-  constructor() {
-    super(AuthMessages.INVALID_CREDENTIALS);
-  }
-}
--- coffee-backend-analysis/src/modules/coffee-sales/exceptions/coffee-salles.exception.ts
+++
@@ -1,7 +0,0 @@
-import { NotFoundException } from '@nestjs/common';
-
-export class CoffeeSaleNotFoundException extends NotFoundException {
-  constructor(id: number) {
-    super(`Coffee sale with id ${id} not found`);
-  }
-}
